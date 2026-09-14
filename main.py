6b45a692210eec0ec0eefd1bad47817ea87400d3
Rian-Averilla <averilla.ads@obmontessori.edu.ph>
2026-09-15 07:21:27 +0800

    practice

---
+++ main.py
@@ -0,0 +1,28 @@
+from pyscript import display
+#integer
+
+# 0 /
+# Using comma /
+#using underscore works
+
+fullname = 'Juan Dela Cruz' # string 
+
+display(f'Hi! I am {fullname} and I am {ag3_s} years old.', target="result")
+
+_cool = "Cool"
+b = ["one", "two", "three"]
+Wow = (1, 2, 3)
+
+display(type(_cool))
+display(type(b))
+display(type(Wow))
+
+display((_cool))
+display((b))
+display((Wow))
+
+a = 0
+f = 0.1
+
+display(bool(a))
+display(bool(f))
